03a6e80a48baef64d24bdb25b0ae5beb9d832b74
Jonathan Moore Liles <j.liles@unix.net>
2008-05-05 11:24:54 -0500

    Add omitted annoation region source files!

---
+++ Timeline/Annotation_Region.C
@@ -0,0 +1,117 @@
+
+/*******************************************************************************/
+/* Copyright (C) 2008 Jonathan Moore Liles                                     */
+/*                                                                             */
+/* This program is free software; you can redistribute it and/or modify it     */
+/* under the terms of the GNU General Public License as published by the       */
+/* Free Software Foundation; either version 2 of the License, or (at your      */
+/* option) any later version.                                                  */
+/*                                                                             */
+/* This program is distributed in the hope that it will be useful, but WITHOUT */
+/* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       */
+/* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for   */
+/* more details.                                                               */
+/*                                                                             */
+/* You should have received a copy of the GNU General Public License along     */
+/* with This program; see the file COPYING.  If not,write to the Free Software */
+/* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+/*******************************************************************************/
+
+#include <FL/Fl_Tooltip.H>
+#include <FL/fl_ask.H>
+
+#include "Annotation_Region.H"
+
+void
+Annotation_Region::get ( Log_Entry &e ) const
+{
+    Sequence_Region::get( e );
+
+    e.add( ":label", _label );
+}
+
+void
+Annotation_Region::set ( Log_Entry &e )
+{
+    Sequence_Region::set( e );
+
+    for ( int i = 0; i < e.size(); ++i )
+    {
+        const char *s, *v;
+
+        e.get( i, &s, &v );
+
+        if ( ! strcmp( s, ":label" ) )
+            name( v );
+    }
+
+//            timeline->redraw();
+}
+
+Annotation_Region::Annotation_Region ( Sequence *track, nframes_t when, const char *name )
+{
+    _track = track;
+
+    _r->offset = when;
+
+    /* FIXME: hack */
+    _r->end = 400;
+
+    _label = strdup( name );
+
+    log_create();
+}
+
+Annotation_Region::Annotation_Region ( const Annotation_Region &rhs )
+{
+    _r->offset = rhs._r->offset;
+    _label = strdup( rhs._label );
+}
+
+
+Annotation_Region::~Annotation_Region ( )
+{
+    if ( _label ) free( _label );
+    log_destroy();
+}
+
+
+
+void
+Annotation_Region::draw_box ( void )
+{
+    Sequence_Region::draw_box();
+}
+
+void
+Annotation_Region::draw ( void )
+{
+    draw_label( _label, (Fl_Align)(FL_ALIGN_LEFT | FL_ALIGN_INSIDE | FL_ALIGN_CLIP ) );
+}
+
+
+int
+Annotation_Region::handle ( int m )
+{
+    Logger _log( this );
+
+    if ( m == FL_PUSH && Fl::test_shortcut( FL_BUTTON3 ) && ! Fl::event_shift() )
+    {
+        const char *s = fl_input( "New name for mark:", name() );
+
+        if ( s )
+            name( s );
+
+        return 1;
+    }
+
+    int r = Sequence_Region::handle( m );
+
+    if ( m == FL_RELEASE )
+    {
+        _track->sort();
+        timeline->redraw();
+    }
+
+    return r;
+}

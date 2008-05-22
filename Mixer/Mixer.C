f9fb837c58434f39765da1d78b2cfad6fa9677da
Jonathan Moore Liles <j.liles@unix.net>
2008-05-22 10:32:26 -0500

    Add missing GPL headers.

--- Mixer/Mixer.C
+++ Mixer/Mixer.C
@@ -17,7 +17,6 @@
 /* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 /*******************************************************************************/
 
-
 #include <FL/Fl.H>
 #include <FL/Fl_Single_Window.H>
 #include <FL/Fl_Single_Window.H>
--- Timeline/Loggable.C
+++ Timeline/Loggable.C
@@ -17,6 +17,7 @@
 /* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 /*******************************************************************************/
 
+
 #define _LOGGABLE_C
 #include "Loggable.H"
 #undef _LOGABLE_C

1b3994a5f1e173fd40d6c1e456374e0441db8c5b
Jonathan Moore Liles <j.liles@unix.net>
2013-10-04 18:10:30 -0700

    Mixer/Panner: Add 3 meter range setting.

--- mixer/src/Panner.C
+++ mixer/src/Panner.C
@@ -44,7 +44,7 @@ Panner::Panner ( int X, int Y, int W, int H, const char *L ) :
 //    _projection = POLAR;
     _points.push_back( Point( 1, 0 ) );
     
-    static int ranges[] = { 1,5,10,15 };
+    static float ranges[] = { 1,3,5,10,15 };
     { Fl_Choice *o = _range_choice = new Fl_Choice(X + 40,Y + H - 18,75,18,"Range:");
         o->box(FL_UP_FRAME);
         o->down_box(FL_DOWN_FRAME);
@@ -52,9 +52,10 @@ Panner::Panner ( int X, int Y, int W, int H, const char *L ) :
         o->labelsize(9);
         o->align(FL_ALIGN_LEFT);
         o->add("1 Meter",0,0,&ranges[0]);
-        o->add("5 Meters",0,0,&ranges[1]);
-        o->add("10 Meters",0,0,&ranges[2]);
-        o->add("15 Meters",0,0,&ranges[3]);
+        o->add("3 Meters",0,0,&ranges[1]);
+        o->add("5 Meters",0,0,&ranges[2]);
+        o->add("10 Meters",0,0,&ranges[3]);
+        o->add("15 Meters",0,0,&ranges[4]);
         o->value(_range_mode);
         o->callback( cb_mode, this );
     }
